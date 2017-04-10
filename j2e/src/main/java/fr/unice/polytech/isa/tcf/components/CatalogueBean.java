cfe192456b25c9c45448ef6f03325b22e9ed5ac9
Sébastien Mosser <mosser@i3s.unice.fr>
2017-04-10 17:49:28 +0200

    fix indentation in Catalogue Impl

--- j2e/src/main/java/fr/unice/polytech/isa/tcf/components/CatalogueBean.java
+++ j2e/src/main/java/fr/unice/polytech/isa/tcf/components/CatalogueBean.java
@@ -5,6 +5,7 @@ import fr.unice.polytech.isa.tcf.entities.Cookies;
 
 import javax.ejb.Stateless;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,21 +13,18 @@ import java.util.Set;
 public class CatalogueBean implements CatalogueExploration {
 
 
-	@Override
-	public Set<Cookies> listPreMadeRecipes() {
-		Set result = new HashSet<Cookies>();
-		for(Cookies c: Cookies.values())
-			result.add(c);
-		return result;
-	}
+    @Override
+    public Set<Cookies> listPreMadeRecipes() {
+        return new HashSet<>(Arrays.asList(Cookies.values()));
+    }
 
-	@Override
-	public Set<Cookies> exploreCatalogue(String regexp) {
-		Set<Cookies> result = new HashSet<>();
-		for(Cookies c: Cookies.values()) {
-			if(c.name().contains(regexp))
-				result.add(c);
-		}
-		return result;
-	}
+    @Override
+    public Set<Cookies> exploreCatalogue(String regexp) {
+        Set<Cookies> result = new HashSet<>();
+        for(Cookies c: Cookies.values()) {
+            if(c.name().contains(regexp))
+                result.add(c);
+        }
+        return result;
+    }
 }
